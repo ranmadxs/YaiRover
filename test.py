a7c870afb87992817cafcd6f612212f76397e658
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-08-15 20:38:25 -0300

    i2c sample

--- test.py
+++ test.py
@@ -6,4 +6,4 @@ from svc.YaiCommunicator import I2c
 log.info("Send command I2C")
 
 yaiCommunicator = I2c()
-yaiCommunicator.sendCommand("SERIAL,100001,1001,0,0,10003,None,None,None")
+yaiCommunicator.sendCommand("I2C,100001,1001,0,0,10002,None,None,None")
