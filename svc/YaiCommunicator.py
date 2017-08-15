32ee8f8a03fb7baf38e9e8af3cdedba0d792dcd9
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-08-15 20:59:46 -0300

    receive i2c

--- svc/YaiCommunicator.py
+++ svc/YaiCommunicator.py
@@ -10,7 +10,7 @@ class I2c():
     
     MAX_I2C_COMAND = 32
     MAX_I2C_CONTENT = 26
-    LCD_ADDR = 0x09
+    CLIENT_ADDR = 0x09
     I2C_DEV = 0
     YAI_COMMAND_TYPE_I2C = "I2C"
     
@@ -34,8 +34,10 @@ class I2c():
         cmd1 = self.buildI2Cpackage(cmd1, totalParts, 1)
         print cmd1
         messageInBytes = self.StringToBytes(cmd1)
-        bus.write_i2c_block_data(self.LCD_ADDR, 0, messageInBytes)
-        
+        bus.write_i2c_block_data(self.CLIENT_ADDR, 0, messageInBytes)
+        time.sleep(0.2)
+        data_received_from_Arduino = bus.read_i2c_block_data(self.CLIENT_ADDR, 0,32)
+        print(data_received_from_Arduino)
         time.sleep(.25)
         if (totalParts > 1) :
             cmd2 = cmd[self.MAX_I2C_CONTENT:]
@@ -43,5 +45,6 @@ class I2c():
             cmd2 = self.buildI2Cpackage(cmd2, totalParts, 2)
             print cmd2
             messageInBytes = self.StringToBytes(cmd2)
-            bus.write_i2c_block_data(self.LCD_ADDR, 0, messageInBytes)
-            
+            bus.write_i2c_block_data(self.CLIENT_ADDR, 0, messageInBytes)
+            data_received_from_Arduino = bus.read_i2c_block_data(self.CLIENT_ADDR, 0,32)
+            print(data_received_from_Arduino)            
