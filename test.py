951ec7d218dfaeda6d0cb2b1adcf3c1861b33280
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-08-15 19:20:44 -0300

    test

--- test.py
+++ test.py
@@ -3,7 +3,12 @@
 import smbus
 import time
 
+MAX_I2C_COMAND = 32
+MAX_I2C_CONTENT = 26
 LCD_ADDR = 0x09
+I2C_DEV = 0
+YAI_COMMAND_TYPE_I2C = "I2C"
+
 
 def StringToBytes(val):
         retVal = []
@@ -11,10 +16,25 @@ def StringToBytes(val):
                 retVal.append(ord(c))
         return retVal
 
-def SayHello():
-        bus = smbus.SMBus(0)        
-        messageInBytes = StringToBytes("I2C12SERIAL,100001,1001,0,0,1000")
-        #3,None,None,None
+def buildI2Cpackage(command, total, part) :
+    respCommand = "%s%s%s%s" % (YAI_COMMAND_TYPE_I2C, part, total, command);
+    return respCommand
+
+def sendI2CCommand(cmd):
+        bus = smbus.SMBus(I2C_DEV)
+        totalParts = 1;
+        if (len(cmd) > MAX_I2C_CONTENT) :
+            totalParts = 2;
+        cmd1 = cmd[:MAX_I2C_CONTENT]
+        cmd1 = buildI2Cpackage(cmd1, totalParts, 1)
+        messageInBytes = StringToBytes(cmd1)
         bus.write_i2c_block_data(LCD_ADDR, 0, messageInBytes)
+
+        if (totalParts > 1) :
+            toEnd = -1*MAX_I2C_CONTENT
+            cmd2 = cmd[toEnd:]
+            cmd2 = buildI2Cpackage(cmd2, totalParts, 2)
+            messageInBytes = StringToBytes(cmd2)
+            bus.write_i2c_block_data(LCD_ADDR, 0, messageInBytes)
         
-SayHello()
+sendI2CCommand("SERIAL,100001,1001,0,0,10003,None,None,None")
