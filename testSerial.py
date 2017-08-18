cc212dd661b30f5be68e21e05e732e1519c31239
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-08-18 00:08:31 -0300

    prueba test serial

--- testSerial.py
+++ testSerial.py
@@ -4,7 +4,11 @@ Created on 17-08-2017
 @author: esanchez
 '''
 
-from svc.SerialSvc import YaiSerial
+#from svc.SerialSvc import YaiSerial
+import serial
 
-yaiSerial = YaiSerial()
-yaiSerial.send("XDDDD")
+arduino = serial.Serial('/dev/ttyUSB0', 9600)
+arduino.write("0YL69")
+
+#yaiSerial = YaiSerial()
+#yaiSerial.send("XDDDD")
