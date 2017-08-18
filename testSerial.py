56d95b8759a6a8b65ac33494eb8aaca16be9a005
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-08-17 23:38:03 -0300

    Moviendo test

--- testSerial.py
+++ testSerial.py
@@ -4,12 +4,7 @@ Created on 17-08-2017
 @author: esanchez
 '''
 
-import sys
-from SerialSvc import YaiSerial
-sys.path.insert(0, '../svc/SerialSvc')
-
-
-import SerialSvc
+from svc.SerialSvc import YaiSerial
 
 yaiSerial = YaiSerial()
 yaiSerial.send("XDDDD")
