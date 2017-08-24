beb16252b7b21ed6f868a4633233b24de72f9e34
Esanchez Orangepizero <orangepizero@dddls.net>
2017-08-24 04:07:40 +0000

    testSerial

--- testSerial.py
+++ testSerial.py
@@ -7,7 +7,7 @@ Created on 17-08-2017
 #from svc.SerialSvc import YaiSerial
 import serial
 
-arduino = serial.Serial('/dev/ttyS0', 9600)
+arduino = serial.Serial('/dev/ttyS1', 9600)
 arduino.write("Hola mundo desde Orange Pi Zero")
 #yaiSerial = YaiSerial()
 #yaiSerial.send("XDDDD")
