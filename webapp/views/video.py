fcdb87139d31178a4dcb7b603a0e16eceb883d0e
edgar.sanchez <esanchez@redtogreen.com>
2017-08-30 13:13:47 -0300

    Snapshot

--- webapp/views/video.py
+++ webapp/views/video.py
@@ -4,7 +4,7 @@ Created on 25-08-2017
 @author: instala
 '''
 from django.shortcuts import render
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, HttpResponse
 #from django.views.generic.base import View
 from lib.logger import logger as log
 from svc.CameraSvc import VideoCamera
@@ -13,8 +13,9 @@ class VideoController():
     
     def getSnapshot(self, camera):
         frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')     
+        #yield (b'--frame\r\n'
+        #       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        return frame     
     
     def getStream(self, camera):
         while True:
@@ -23,8 +24,11 @@ class VideoController():
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')        
     
     def snapshot(self, request):
-        log.info("Snapshot Web")
-        return StreamingHttpResponse(self.getSnapshot(VideoCamera()), content_type="multipart/x-mixed-replace; boundary=frame")
-    
+        log.info("Snapshot Web")        
+        #videoCamera = VideoCamera()
+        #image_data = videoCamera.get_image()
+        #return HttpResponse(image_data, content_type="image/jpg")
+        return StreamingHttpResponse(self.getSnapshot(VideoCamera()), content_type="image/jpg; boundary=frame")
+        
     def stream(self, request):
         return StreamingHttpResponse(self.getStream(VideoCamera()), content_type='multipart/x-mixed-replace; boundary=frame')
