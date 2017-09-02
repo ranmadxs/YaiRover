3c659b849b9ad8038221a3be452e3e1aa385598d
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-09-02 02:00:35 -0300

    cambiando addresss i2c

--- testI2C.py
+++ testI2C.py
@@ -7,4 +7,5 @@ from roverenum import EnumCommunicator
 log.info("Send command I2C")
 
 yaiCommunicator = I2c()
-yaiCommunicator.sendCommand("I2C,100001,1001,0,0,10002,None,None,None", EnumCommunicator.I2CEnum.I2C_CLIENT_YAI_SERVO.value)
+#yaiCommunicator.sendCommand("I2C,100001,1001,0,0,10002,None,None,None", EnumCommunicator.I2CEnum.I2C_CLIENT_YAI_MOTOR.value)
+yaiCommunicator.sendCommand("I2C,200003,2001,0,20003,None,None,None,None", EnumCommunicator.I2CEnum.I2C_CLIENT_YAI_SERVO.value)
