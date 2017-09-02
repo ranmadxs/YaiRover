b5c22a9b5efebd075b93d4a776c69719a1d50b10
Edgar Sanchez <grineldosanchez@yahoo.es>
2017-09-02 02:47:21 -0300

    I2C test

--- testI2C2S.py
+++ testI2C2S.py
@@ -8,4 +8,4 @@ log.info("Send command I2C")
 
 yaiCommunicator = I2c()
 #yaiCommunicator.sendCommand("I2C,100001,1001,0,0,10002,None,None,None", EnumCommunicator.I2CEnum.I2C_CLIENT_YAI_MOTOR.value)
-yaiCommunicator.sendCommand("I2C,100002,1001,0,None,None,None,None,None", EnumCommunicator.I2CEnum.I2C_CLIENT_YAI_MOTOR.value)
+yaiCommunicator.sendCommand("I2C,200003,2001,0,20003,None,None,None,None", EnumCommunicator.I2CEnum.I2C_CLIENT_YAI_SERVO.value)
